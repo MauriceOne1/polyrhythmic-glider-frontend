b36d56592ee52b5284e7cfe29f4b6b77f97dbf24
Maurizio Querzoli <maurizio.querzoli@gmail.com>
2026-03-17 00:52:36 +0100

    chore: UI

--- src/app/features/home/home-page/home-page.ts
+++ src/app/features/home/home-page/home-page.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AboutSection } from '../about-section/about-section';
 import { HeroSection } from '../hero-section/hero-section';
+import { ContactSection } from "../contact-section/contact-section";
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule, AboutSection, HeroSection],
+  imports: [CommonModule, AboutSection, HeroSection, ContactSection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
