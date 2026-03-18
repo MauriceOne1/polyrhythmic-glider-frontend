e52e0614bce7e8ae227425123d19c1258e96b452
Maurizio Querzoli <maurizio.querzoli@gmail.com>
2026-03-18 15:50:39 +0100

    fix(header): UI

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { NAV_ITEMS } from '../../../shared/utils/site-content';
 
 @Component({
@@ -12,11 +13,23 @@ export class Header {
   readonly navItems = NAV_ITEMS;
   readonly isMenuOpen = signal(false);
 
+  private readonly router = inject(Router);
+  private readonly viewportScroller = inject(ViewportScroller);
+
   toggleMenu(): void {
     this.isMenuOpen.update((value) => !value);
   }
 
   closeMenu(): void {
     this.isMenuOpen.set(false);
+  }
+
+  navigateHome(event?: Event): void {
+    event?.preventDefault();
+    this.closeMenu();
+
+    this.router.navigateByUrl('/').then(() => {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    });
   }
 }
